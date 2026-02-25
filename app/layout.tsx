e0c1971ada48a68125b25614e8f2e85b9257fb64
rashmin <rashminxperience@gmail.com>
2026-02-25 18:29:18 +0545

    added clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const outfitFont = Outfit({
   subsets: ["latin"],
@@ -22,9 +29,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${outfitFont.className} antialiased`}>
-          {children}
-        </body>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <body className={`${outfitFont.className} antialiased`}>
+            {children}
+          </body>
+        </header>
       </html>
     </ClerkProvider>
   );
